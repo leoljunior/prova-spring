1a4c2f7a59501a62105b1e48a93a0571d8628106
Leonardo de Lima Junior <juhhinx@gmail.com>
2022-04-16 13:37:21 -0300

    fisrt commit

---
+++ src/main/java/com/example/LeonardoLima/LeonardoLimaApplication.java
@@ -0,0 +1,13 @@
+package com.example.LeonardoLima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeonardoLimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeonardoLimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LeonardoLima/LeonardoLimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LeonardoLima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeonardoLimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
